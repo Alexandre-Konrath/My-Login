2db72b664e83cd6559540ffcb167f764dc216011
Alexandre de Lima Konrath <160286787+Alexandre-Konrath@users.noreply.github.com>
2024-04-29 17:47:44 -0300

    adding the front-end connection route

--- src/server.js
+++ src/server.js
@@ -1,9 +1,20 @@
-const express = require("express")
-const routes = require("./routes")
+const express = require("express");
+const routes = require("./routes");
 
-const app = express()
+const app = express();
 
-app.use(express.json())
-app.use(routes)
+app.use(express.json());
 
-app.listen(3001)
+// Configurando o middleware para permitir solicitações CORS
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
+  next();
+});
+
+app.use(routes);
+
+app.listen(3001, () => {
+  console.log("Server is running on port 3001");
+});
