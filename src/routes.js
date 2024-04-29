e04626f295ed99a05a4e25025e286c03bf59bd5a
Alexandre de Lima Konrath <160286787+Alexandre-Konrath@users.noreply.github.com>
2024-04-29 17:46:07 -0300

    adding the login route

--- src/routes.js
+++ src/routes.js
@@ -8,4 +8,7 @@ const routes = express.Router()
 routes.get('/users', UserController.index )
 routes.post('/users', UserController.store )
 
+// Rota para autenticação de usuário
+routes.post("/login", UserController.login);
+
 module.exports = routes
